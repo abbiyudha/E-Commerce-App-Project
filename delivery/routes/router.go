01cc389837eaae03dba84e728fcb93665244fd61
abbiyudha <abbiyudha14@gmail.com>
2022-04-10 00:15:18 +0700

    feature auth

--- delivery/routes/router.go
+++ delivery/routes/router.go
@@ -1,10 +1,15 @@
 package routes
 
 import (
+	"ecommerce/delivery/handler/auth"
 	"ecommerce/delivery/handler/user"
 	"ecommerce/delivery/middlewares"
 	"github.com/labstack/echo/v4"
 )
+
+func RegisterAuthPath(e *echo.Echo, ah *auth.AuthHandler) {
+	e.POST("/auth", ah.LoginHandler())
+}
 
 func RegisterPath(e *echo.Echo, uh *user.UserHandler) {
 
